f061ec2f16c1bc7446e8676315e120318e1487fc
anthony <gotbordom@gmail.com>
2025-01-14 12:35:15 -0700

    initial commit with simle hello world and cmake

---
+++ c++_essnetial_training/src/library_catalog_card.cpp
@@ -0,0 +1,12 @@
+// std headers
+#include <iostream>
+
+// third party headers
+
+// project headers
+
+
+auto main(int argc, char** argv) -> int {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
