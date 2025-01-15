dcc8740b3a72400dc5c8c8aa609caf952f67078f
anthony <gotbordom@gmail.com>
2025-01-15 10:52:21 -0700

    simplified the struct, removed constructor, etc.

--- c++_essnetial_training/src/library_catalog_card.cpp
+++ c++_essnetial_training/src/library_catalog_card.cpp
@@ -9,17 +9,9 @@
 // project headers
 
 
-
-// NOTES: I thought about making it a class with getters/ setters, etc. But this is a tutorial and 
-// I don't see a reason to overcomplicate a solution.
+// There is no need to protect anything on this "public library card".
 struct LibraryCatalogCard{
-    LibraryCatalogCard(std::string title, std::string author, std::string publisher, 
-        std::string subject, std::string isbn, std::string world_cat, std::pair<int,int> dewey_decimal, 
-        int publication_year, int aquisition_year, int total_owned, int total_in_stock)
-	    : title_(title), author_(author), publisher_(publisher), subject_(subject), isbn_(isbn), 
-	      world_cat_(world_cat), dewey_decimal_(dewey_decimal), publication_year_(publication_year), 
-	      total_owned_(total_owned), total_in_stock_(total_in_stock) 
-        {}
+    // Could add equality operators, etc.
 
     std::string title_;
     std::string author_;
@@ -37,10 +29,10 @@ struct LibraryCatalogCard{
 auto main(int argc, char** argv) -> int {
 
     // Just creating the struct example
-    auto lib_card = LibraryCatalogCard(
+    auto lib_card = LibraryCatalogCard{
         "My New Title", "Some Author", "By a publishing company", 
         "Horror", "1234567890", "world_cat_value?", 
-        std::pair{100,10}, 1999, 2000, 10, 1);
+        std::pair{100,10}, 1999, 2000, 10, 1};
 
     // printing out some of the data
     std::cout << std::format("Hello my new library card.\n\tTitle: {} \n\tAuthor: {} \n\tSubject: {}", 
