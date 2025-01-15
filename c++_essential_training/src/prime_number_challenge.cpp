b9be2f4b202c36b2dbf3112283b7892ce8b7d999
anthony <gotbordom@gmail.com>
2025-01-15 11:11:49 -0700

    added clang formatting, some vscode settings, and layout for prime num challenge;

---
+++ c++_essential_training/src/prime_number_challenge.cpp
@@ -0,0 +1,22 @@
+// std headers
+
+// third party headers
+
+// project headers
+
+
+// Challange: calculate the first 25 prime numbers
+// Function: is_number_prime(int) -> bool
+// Function: calculate_prime_numbers(int n) -> std::vector<int>
+auto is_number_prime(int number) -> int {
+
+
+    return number;
+}
+
+
+auto main(int argc, char* argv[]) -> {
+
+
+    return 0;
+}
