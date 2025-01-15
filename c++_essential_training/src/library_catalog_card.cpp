d67dec3eb5f5d5a8506e12508358419e031b2a22
anthony <gotbordom@gmail.com>
2025-01-15 11:26:10 -0700

    setup clang format and formatted files

--- c++_essential_training/src/library_catalog_card.cpp
+++ c++_essential_training/src/library_catalog_card.cpp
@@ -1,42 +1,52 @@
 // std headers
+#include <format>
 #include <iostream>
 #include <string>
-#include <format>
 #include <utility>
 
 // third party headers
 
 // project headers
 
-
 // There is no need to protect anything on this "public library card".
-struct LibraryCatalogCard{
-    // Could add equality operators, etc.
+struct LibraryCatalogCard
+{
+  // Could add equality operators, etc.
 
-    std::string title_;
-    std::string author_;
-    std::string publisher_;
-    std::string subject_;
-    std::string isbn_;
-    std::string world_cat_;
-    std::pair<int, int> dewey_decimal_;
-    int publication_year_;
-    int aquisition_year_;
-    int total_owned_;
-    int total_in_stock_;
+  std::string title_;
+  std::string author_;
+  std::string publisher_;
+  std::string subject_;
+  std::string isbn_;
+  std::string world_cat_;
+  std::pair<int, int> dewey_decimal_;
+  int publication_year_;
+  int aquisition_year_;
+  int total_owned_;
+  int total_in_stock_;
 };
 
-auto main(int argc, char** argv) -> int {
+auto main(int argc, char** argv) -> int
+{
+  // Just creating the struct example
+  auto lib_card = LibraryCatalogCard{"My New Title",
+                                     "Some Author",
+                                     "By a publishing company",
+                                     "Horror",
+                                     "1234567890",
+                                     "world_cat_value?",
+                                     std::pair{100, 10},
+                                     1999,
+                                     2000,
+                                     10,
+                                     1};
 
-    // Just creating the struct example
-    auto lib_card = LibraryCatalogCard{
-        "My New Title", "Some Author", "By a publishing company", 
-        "Horror", "1234567890", "world_cat_value?", 
-        std::pair{100,10}, 1999, 2000, 10, 1};
+  // printing out some of the data
+  std::cout << std::format(
+                   "Hello my new library card.\n\tTitle: {} \n\tAuthor: {} "
+                   "\n\tSubject: {}",
+                   lib_card.title_, lib_card.author_, lib_card.subject_)
+            << std::endl;
 
-    // printing out some of the data
-    std::cout << std::format("Hello my new library card.\n\tTitle: {} \n\tAuthor: {} \n\tSubject: {}", 
-        lib_card.title_, lib_card.author_, lib_card.subject_) << std::endl;
-
-    return 0;
+  return 0;
 }
--- c++_essential_training/src/prime_number_challenge.cpp
+++ c++_essential_training/src/prime_number_challenge.cpp
@@ -1,22 +1,18 @@
 // std headers
+#include <vector>
 
 // third party headers
 
 // project headers
 
-
 // Challange: calculate the first 25 prime numbers
 // Function: is_number_prime(int) -> bool
 // Function: calculate_prime_numbers(int n) -> std::vector<int>
-auto is_number_prime(int number) -> int {
 
-
-    return number;
+auto is_number_prime(int number) -> int { return number; }
+auto generate_n_prime_numbers(int n) -> std::vector<int>
+{
+  return std::vector<int>{};
 }
 
-
-auto main(int argc, char* argv[]) -> {
-
-
-    return 0;
-}
+auto main(int argc, char* argv[]) -> int { return 0; }
